5337ccaec3e9520fd60057345a13e2fa09ac5dc6
Phil <philip.schaefer0@gmail.com>
2025-01-06 20:28:07 +0100

    feat - placeholder pages

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Fa from 'svelte-fa';
+  import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="hero">
+  <div class="hero-content">
+    <h1>News</h1>
+  </div>
+</div>
+
+<div class="hero">
+  <div class="hero-content">
+    <h1>Changelog</h1>
+  </div>
+</div>
+<div class="hero">
+  <div class="hero-content max-w-md alert alert-info h-auto">
+    <Fa icon={faInfoCircle} size=lg />
+    <span>The changelog for the frontend can be found <a class="link" target="noreferrer" href="https://github.com/Resonite-Community-Projects/resonite-events-frontend/commits/main/">on GitHub</a> and for the backend source can be found <a class="link" target="noreferrer" href="https://github.com/Resonite-Community-Projects/community_events.resonite/commits/stable/">here</a>.</span>
+  </div>
+</div>
---
+++ src/routes/streams/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Fa from 'svelte-fa';
+  import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
+</script>
+
+<div class="hero">
+  <div class="hero-content">
+    <h1>Scheduled Streams</h1>
+  </div>
+</div>
+<div class="hero">
+  <div class="hero-content max-w-md alert alert-info h-auto">
+    <Fa icon={faInfoCircle} size=lg />
+    <span>This page is not ready yet and needs to be implemented in the backend first. Use the <a class="link" target="noreferrer" href="https://resonite-communities.com/streams">old page</a> in the meantime.</span>
+  </div>  
+</div>
